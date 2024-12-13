d702a498dc2376955ed83f6e9e7d3f4c25eb4f75
PrithS24 <prithasaha2022@gmail.com>
2024-12-14 04:04:49 +0600

    Updated feature X or fixed bug Y

---
+++ frontend/src/app/Homepage/page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HomePage = () =>{
+    return(
+        <div>
+            page
+        </div>
+    )
+}
+export default HomePage
---
+++ frontend/src/app/layout.js
@@ -0,0 +1,32 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "CUETbook",
+  description: "A CUET Community based social-media website",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children} 
+        {/* every page will get rendered here */}
+      </body>
+    </html>
+  );
+}
+// server side rendering 
+// next.js is server side rendering and react is client side
