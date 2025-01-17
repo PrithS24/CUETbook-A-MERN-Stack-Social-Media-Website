f8251125075e9a3c9581a428d5570a5e45662040
PrithS24 <prithasaha2022@gmail.com>
2025-01-17 15:52:52 +0600

    Updated

---
+++ backend/model/User.js
@@ -0,0 +1,7 @@
+// const mongoose=require('mongoose');
+
+// const userSchema = new mongoose.Schema({
+//     username:{type:String, required:true },
+//     email:{type:Strung,required:true, unique:true},
+//     password:{type:String, default:null}
+// })
