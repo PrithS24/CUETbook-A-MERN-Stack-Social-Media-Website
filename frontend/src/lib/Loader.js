65e1f06d43c4fd63df797efc1a81d4d474a0643a
PrithS24 <prithasaha2022@gmail.com>
2025-02-09 16:19:25 +0600

    Updated

--- frontend/src/lib/Loader.js
+++ frontend/src/lib/Loader.js
@@ -20,19 +20,19 @@ export default function Loader() {
   return (
     <div className="fixed inset-0 flex flex-col gap-4 items-center justify-center bg-blue-600 dark:bg-blue-200 z-50">
       <div className="relative  w-40 h-40">
-      <motion.img
-          src="/your-image-path.png"  // Replace with the correct image path
-          alt="Loader Icon"
-          className="w-full h-full object-contain"
+        <motion.svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          className="w-full h-full text-white"
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-        />
+        >
           <path
-            fill="green"
+            fill="currentColor"
             d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
           />
-        {/* </motion.img> */}
+        </motion.svg>
       </div>
       <div className="flex space-x-3">
         {[0, 1, 2].map((index) => (
